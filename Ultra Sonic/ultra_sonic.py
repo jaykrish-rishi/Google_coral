ff61ca4d25e4b1fee548197bb6a4e94ccf64df06
JAIKISHORE G <99666021+jaykrish-rishi@users.noreply.github.com>
2023-04-06 14:43:36 +0530

    Add files via upload

---
+++ Ultra Sonic/ultra_sonic.py
@@ -0,0 +1,47 @@
+from periphery import GPIO
+import time
+
+trig = GPIO("/dev/gpiochip2", 13, "out")  # pin 37 trig
+echo = GPIO("/dev/gpiochip4", 13, "in")  # pin 36 echo
+
+trig.write(False)
+print("Calibrating...")
+time.sleep(2)
+
+print ("place the object")
+
+try :
+    while True :
+        trig.write(True)
+        time.sleep(0.00001)
+        trig.write(False)
+
+        while echo.read() == 0:
+            pluse_start = time.time()
+
+        while echo,read() == 1 :
+            pluse_end = time.time()
+
+        pluse_duration = pluse_end - pluse_start
+
+
+        distance = pluse_duration * 17150
+
+        distance = round(distance+1.15 , 2)
+
+        if distance<=20 and distance >=5:
+            print("distance : %f cm"%(distance))
+            i=1
+
+        if distance >20 and i==1 :
+            print (" place the object ")
+            i=0
+
+        time.sleep(2)
+
+except KeyboardInterrupt :
+    trig.write(False)
+    trig.close()
+    echo.close()
+            
+        
