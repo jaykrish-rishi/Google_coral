67271ae4ee5830f6a387fe30aafd696e13ac471e
JAIKISHORE G <99666021+jaykrish-rishi@users.noreply.github.com>
2023-04-17 07:21:37 +0000

    error-solved

--- servo_motor_interfacing/servo_main.py
+++ servo_motor_interfacing/servo_main.py
@@ -1,11 +1,11 @@
 from periphery import PWM
 import time
 
-# Open PWM chip 3, channel 0
-pwm3 = PWM(2, 0)
+# Open PWM chip 0, channel 0
+pwm = PWM(0, 0)
 
 # Set frequency to 50Hzs
-pwm3.frequency = 50
+pwm.frequency = 50
 
 # in servo motor,
 # 1ms pulse for 0 degree (LEFT)
@@ -17,23 +17,23 @@ pwm3.frequency = 50
 # duty cycle for 90 degree = (1.5/20)*100 = 7.5%
 # duty cycle for 180 degree = (2/20)*100 = 10%
 
-pwm3.enable()
+pwm.enable()
 
 
 try:
 	while True:
-		pwm3.duty_cycle = 0.25 # Change duty cycle to .5ms 
+		pwm.duty_cycle = 0.05 # Change duty cycle to .5ms 
 		time.sleep(1.5)
-		pwm3.duty_cycle = 0.5 # Change duty cycle to 1.5ms
+		pwm.duty_cycle = 0.075 # Change duty cycle to 1.5ms
 		time.sleep(1.5)
-		pwm3.duty_cycle = 0.75 # Change duty cycle to 2.5ms
+		pwm.duty_cycle = 0.1 # Change duty cycle to 2.5ms
 		time.sleep(1.5)
 		print("in while loop")
 		time.sleep(2)
 
 
 except KeyboardInterrupt:
-	pwm3.close()
+	pwm.close()
 
 finally :
-	pwm3.close()
+	pwm.close()
