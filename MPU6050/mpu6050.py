92a90fb4aeee46522d0e51ea3159baba622dc91b
JAIKISHORE G <99666021+jaykrish-rishi@users.noreply.github.com>
2023-04-17 06:23:08 +0000

    error-solved

--- MPU6050/mpu6050.py
+++ MPU6050/mpu6050.py
@@ -52,7 +52,7 @@ GzCal=0
 def InitMPU():
 
     #bus.write_byte_data(Device_Address, DIV, 7)
-    msgs = [I2C.Message([DIV]), I2C.Message([0x07])]
+    msgs = [I2C.Message([DIV ,0x07])]
     i2c.transfer(Device_Address, msgs)
 
     #bus.write_byte_data(Device_Address, PWR_M, 1)
